9badbfa6c2ded3ab104623bac9866efcdd545882
bill_zhong <bill_zhong@perfectcorp.com>
2022-07-28 15:25:59 +0800

    Add google auth

---
+++ packages/auth_repository/lib/src/auth_repository.dart
@@ -0,0 +1,52 @@
+import 'dart:ffi';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+abstract class AuthRepositoryImpl {
+  Future<bool> signInWithGoogle();
+  Future<bool> signInWithGoogleCheck();
+  Future<void> signOut();
+}
+
+class AuthRepository implements AuthRepositoryImpl {
+  AuthRepository()
+      : _firebaseAuth = FirebaseAuth.instance,
+        _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+  @override
+  Future<bool> signInWithGoogleCheck() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    if (googleUser == null) {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  Future<bool> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+    if (googleUser == null) {
+      return false;
+    }
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await _firebaseAuth.signInWithCredential(credential);
+
+    return true;
+  }
+
+  @override
+  Future<void> signOut() async {
+    Future.wait([
+      _firebaseAuth.signOut(),
+      _googleSignIn.signOut(),
+    ]);
+  }
+}
